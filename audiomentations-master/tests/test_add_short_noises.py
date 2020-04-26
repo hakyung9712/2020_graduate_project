6209f20b25b99506e4430f74e71cab6d1e0e77ca
hakyu <hakyungsong9712@gmail.com>
2020-04-26 23:27:46 +0900

    1

---
+++ audiomentations-master/tests/test_add_short_noises.py
@@ -0,0 +1,151 @@
+import json
+import os
+import unittest
+
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from audiomentations import calculate_rms
+from audiomentations.augmentations.transforms import AddShortNoises
+from audiomentations.core.composition import Compose
+from demo.demo import DEMO_DIR
+
+
+class TestAddShortNoises(unittest.TestCase):
+    def test_add_short_noises(self):
+        sample_rate = 16000
+        samples = np.sin(np.linspace(0, 440 * 2 * np.pi, 9 * sample_rate)).astype(
+            np.float32
+        )
+        rms_before = calculate_rms(samples)
+        augmenter = Compose(
+            [
+                AddShortNoises(
+                    sounds_path=os.path.join(DEMO_DIR, "short_noises"),
+                    min_time_between_sounds=2.0,
+                    max_time_between_sounds=8.0,
+                    p=1.0,
+                )
+            ]
+        )
+        samples_out = augmenter(samples=samples, sample_rate=sample_rate)
+        self.assertEqual(samples_out.dtype, np.float32)
+        self.assertEqual(samples_out.shape, samples.shape)
+
+        rms_after = calculate_rms(samples_out)
+        self.assertGreater(rms_after, rms_before)
+
+    def test_input_shorter_than_noise(self):
+        """
+        Verify correct behavior when the input sound is shorter than the added noise sounds.
+        """
+        sample_rate = 16000
+        samples = np.sin(np.linspace(0, 440 * 2 * np.pi, 500)).astype(np.float32)
+        rms_before = calculate_rms(samples)
+        augmenter = Compose(
+            [
+                AddShortNoises(
+                    sounds_path=os.path.join(DEMO_DIR, "short_noises"),
+                    min_time_between_sounds=0.00001,
+                    max_time_between_sounds=0.00002,
+                    p=1.0,
+                )
+            ]
+        )
+        samples_out = augmenter(samples=samples, sample_rate=sample_rate)
+        self.assertEqual(samples_out.dtype, np.float32)
+        self.assertEqual(samples_out.shape, samples.shape)
+
+        rms_after = calculate_rms(samples_out)
+        self.assertGreater(rms_after, rms_before)
+
+    def test_add_silence(self):
+        """Check that AddShortNoises does not crash if a noise is completely silent."""
+        sample_rate = 44100
+        samples = np.sin(np.linspace(0, 440 * 2 * np.pi, 6000)).astype(np.float32)
+        augmenter = Compose(
+            [
+                AddShortNoises(
+                    sounds_path=os.path.join(DEMO_DIR, "silence"),
+                    min_time_between_sounds=0.001,
+                    max_time_between_sounds=0.002,
+                    p=1.0,
+                )
+            ]
+        )
+        augmenter.transforms[0].parameters = {
+            "should_apply": True,
+            "sounds": [
+                {
+                    "fade_in_time": 0.04257633246838298,
+                    "start": -0.00013191289693534575,
+                    "end": 0.8071696744046519,
+                    "fade_out_time": 0.07119110196424423,
+                    "file_path": os.path.join(DEMO_DIR, "silence", "silence.wav"),
+                    "snr_in_db": 19.040001423519563,
+                }
+            ],
+        }
+        augmenter.freeze_parameters()
+
+        samples_out = augmenter(samples=samples, sample_rate=sample_rate)
+        self.assertEqual(samples_out.dtype, np.float32)
+        self.assertEqual(samples_out.shape, samples.shape)
+
+    def test_too_long_fade_time(self):
+        """Check that a too long fade time does not result in an exception."""
+        sample_rate = 16000
+        samples = np.sin(np.linspace(0, 440 * 2 * np.pi, 9 * sample_rate)).astype(
+            np.float32
+        )
+        rms_before = calculate_rms(samples)
+        augmenter = Compose(
+            [
+                AddShortNoises(
+                    sounds_path=os.path.join(DEMO_DIR, "short_noises"),
+                    min_time_between_sounds=2.0,
+                    max_time_between_sounds=8.0,
+                    min_fade_in_time=0.9,
+                    max_fade_in_time=0.99,
+                    min_fade_out_time=0.9,
+                    max_fade_out_time=0.99,
+                    p=1.0,
+                )
+            ]
+        )
+        samples_out = augmenter(samples=samples, sample_rate=sample_rate)
+        self.assertEqual(samples_out.dtype, np.float32)
+        self.assertEqual(samples_out.shape, samples.shape)
+
+        rms_after = calculate_rms(samples_out)
+        self.assertGreater(rms_after, rms_before)
+
+    def test_serialize_parameters(self):
+        transform = AddShortNoises(
+            sounds_path=os.path.join(DEMO_DIR, "background_noises"), p=1.0
+        )
+        samples = np.random.normal(0, 1, size=1024).astype(np.float32)
+        transform.randomize_parameters(samples, sample_rate=16000)
+        json.dumps(transform.serialize_parameters())
+
+    def test_frozen_parameters(self):
+        sample_rate = 16000
+        samples = np.sin(np.linspace(0, 440 * 2 * np.pi, 9 * sample_rate)).astype(
+            np.float32
+        )
+        augmenter = Compose(
+            [
+                AddShortNoises(
+                    sounds_path=os.path.join(DEMO_DIR, "short_noises"),
+                    min_time_between_sounds=2.0,
+                    max_time_between_sounds=8.0,
+                    p=1.0,
+                )
+            ]
+        )
+        samples_out1 = augmenter(samples=samples, sample_rate=sample_rate)
+
+        augmenter.freeze_parameters()
+        samples_out2 = augmenter(samples, sample_rate)
+
+        assert_array_equal(samples_out1, samples_out2)
---
+++ audiomentations-master/tests/test_pitch_shift.py
@@ -0,0 +1,39 @@
+import unittest
+from copy import deepcopy
+
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from audiomentations.augmentations.transforms import PitchShift
+from audiomentations.core.composition import Compose
+
+
+class TestPitchShift(unittest.TestCase):
+    def test_apply_pitch_shift(self):
+        samples = np.zeros((512,), dtype=np.float32)
+        sample_rate = 16000
+        augmenter = Compose([PitchShift(min_semitones=-2, max_semitones=-1, p=1.0)])
+        samples = augmenter(samples=samples, sample_rate=sample_rate)
+
+        self.assertEqual(samples.dtype, np.float32)
+        self.assertEqual(len(samples), 512)
+
+    def test_freeze_parameters(self):
+        """
+        Test that the transform can freeze its parameters, e.g. to apply the effect with the
+        same parameters to multiple sounds.
+        """
+        samples = np.sin(np.linspace(0, 440 * 2 * np.pi, 8000)).astype(np.float32)
+        sample_rate = 16000
+        augmenter = Compose([PitchShift(min_semitones=1, max_semitones=12, p=1.0)])
+
+        first_samples = augmenter(samples=samples, sample_rate=sample_rate)
+        first_parameters = deepcopy(augmenter.transforms[0].parameters)
+
+        augmenter.transforms[0].min_semitones = -12
+        augmenter.transforms[0].max_semitones = -1
+        augmenter.transforms[0].are_parameters_frozen = True
+        second_samples = augmenter(samples=samples, sample_rate=sample_rate)
+
+        self.assertEqual(first_parameters, augmenter.transforms[0].parameters)
+        assert_array_equal(first_samples, second_samples)
